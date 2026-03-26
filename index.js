024ffe5122cd4b4d2609f3cca4945357a808bfe6
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-03-26 12:10:41 +0300

    bot with btns

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 
 const bot = new Telegraf('8787457881:AAGeAQgnbpwsxBOAYVa3GkIBF_MPr1QEgRk')
 
+const movies = ['Inception', 'Interstellar', 'The Matrix', 'Fight Club']
+
 bot.start((ctx) => {
   ctx.reply(
-    '🎬 Привет! Я помогу тебе выбрать случайный фильм! \nНапиши /random',
+    '🎬 Привет! Я помогу тебе выбрать фильм!',
+    Markup.keyboard([
+      ['🎲 Случайный фильм'],
+      ['🎭 По жанру', '🔥 Популярное'],
+    ]).resize(),
   )
 })
 
-bot.command('random', (ctx) => {
-  const movies = ['Mulholland Dr.', 'Parasite', 'Oppenheimer', 'Fight Club']
-
+bot.hears('🎲 Случайный фильм', (ctx) => {
   const random = movies[Math.floor(Math.random() * movies.length)]
-  ctx.reply(`Попробуй посмотреть: ${random}`)
+  ctx.reply(`🎥 Посмотри: ${random}`)
+})
+
+bot.hears('🎭 По жанру', (ctx) => {
+  ctx.reply('Скоро можно будет выбирать жанры 🎭')
+})
+
+bot.hears('🔥 Популярное', (ctx) => {
+  ctx.reply('Скоро покажу популярные фильмы 🔥')
 })
 
 bot.launch()
