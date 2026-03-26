9c6e6a8e403785b284e23965a21ba72322e1caba
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-03-26 16:32:59 +0300

    filtered by genre

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const { Telegraf, Markup } = require('telegraf')
 
 const bot = new Telegraf('8787457881:AAGeAQgnbpwsxBOAYVa3GkIBF_MPr1QEgRk')
 
-const movies = ['Inception', 'Interstellar', 'The Matrix', 'Fight Club']
+const movies = [
+  { title: '2001: A Space Odyssey', genre: 'sci-fi' },
+  { title: 'Blade Runner', genre: 'sci-fi' },
+  { title: 'The Terminator', genre: 'sci-fi' },
+  { title: 'Anora', genre: 'drama' },
+  { title: 'There Will Be Blood', genre: 'drama' },
+  { title: 'Love Exposure', genre: 'drama' },
+  { title: 'Greener Grass', genre: 'comedy' },
+  { title: 'Monty Python and the Holy Grail', genre: 'comedy' },
+  { title: 'It Follows', genre: 'horror' },
+  { title: 'Prince of Darkness', genre: 'horror' },
+  { title: 'The Fly', genre: 'horror' },
+]
 
 bot.start((ctx) => {
   ctx.reply(
@@ -20,8 +32,38 @@ bot.hears('🎲 Случайный фильм', (ctx) => {
 })
 
 bot.hears('🎭 По жанру', (ctx) => {
-  ctx.reply('Скоро можно будет выбирать жанры 🎭')
+  ctx.reply(
+    'Выбери жанр:',
+    Markup.keyboard([
+      ['😂 Комедия', '😱 Ужасы'],
+      ['🚀 Sci-Fi', '🎭 Драма'],
+      ['⬅️ Назад'],
+    ]).resize(),
+  )
 })
+
+bot.hears('😂 Комедия', (ctx) => sendByGenre(ctx, 'comedy'))
+bot.hears('😱 Ужасы', (ctx) => sendByGenre(ctx, 'horror'))
+bot.hears('🚀 Sci-Fi', (ctx) => sendByGenre(ctx, 'sci-fi'))
+bot.hears('🎭 Драма', (ctx) => sendByGenre(ctx, 'drama'))
+
+bot.hears('⬅️ Назад', (ctx) => {
+  ctx.reply(
+    '🎬 Главное меню:',
+    Markup.keyboard([['🎲 Случайный фильм'], ['🎭 По жанру']]).resize(),
+  )
+})
+
+function sendByGenre(ctx, genre) {
+  const filtered = movies.filter((m) => m.genre === genre)
+
+  if (filtered.length === 0) {
+    return ctx.reply('Нет фильмов 😢')
+  }
+
+  const random = filtered[Math.floor(Math.random() * filtered.length)]
+  ctx.reply(`🎥 Попробуй: ${random.title}`)
+}
 
 bot.hears('🔥 Популярное', (ctx) => {
   ctx.reply('Скоро покажу популярные фильмы 🔥')
